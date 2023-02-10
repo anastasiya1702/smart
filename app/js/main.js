67798458fc73455a1793cfdc50fa05a23a208687
anastasiya1702 <voronovanastya988@gmail.com>
2023-02-10 16:55:27 +0200

    added index

---
+++ app/js/main.js
@@ -0,0 +1,17 @@
+$(function () {
+
+  $('.top-slider__inner').slick({
+    dots: true,
+    arrows: false,
+    fade: true,
+    autoplay: true,
+    slidesToShow: 1,
+    autoplaySpeed: 2000,
+  });
+
+});
+
+  
+    
+      
+        
