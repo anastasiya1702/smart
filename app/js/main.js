c2550726e1500e4553ef6fdb2ac3d43b87f8d1ea
anastasiya1702 <voronovanastya988@gmail.com>
2023-02-28 18:10:56 +0200

    added product-list 1

--- app/js/main.js
+++ app/js/main.js
@@ -1,12 +1,33 @@
 $(function () {
 
+  $('.filter-price__input').ionRangeSlider({
+    type: "double",
+    prefix: "$",
+    onStart: function (data) {
+      $('.filter-price__from').text(data.from);
+      $('.filter-price__to').text(data.to);
+    },
+    onChange: function (data) {
+      $('.filter-price__from').text(data.from);
+      $('.filter-price__to').text(data.to);
+    },
+  })
+
   $('.top-slider__inner').slick({
     dots: true,
     arrows: false,
     fade: true,
     autoplay: true,
     slidesToShow: 1,
-    autoplaySpeed: 2000,
+    autoplaySpeed: 600,
+  });
+
+  $(".star").rateYo({
+    starWidth: "12px",
+    normalFill: "#d6d6d6",
+    ratedFill: "#ffcc00",
+    readOnly: true,
+    spacing: "5px"
   });
 
 });
